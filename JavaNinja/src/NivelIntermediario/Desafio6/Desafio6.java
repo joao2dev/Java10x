446160d86231f8a06170e65ce1fad5857d55bd1f
joao2dev <132752092+joao2dev@users.noreply.github.com>
2026-01-24 12:10:15 -0300

    Implement interactive menu for ninja management
    Added a menu loop for ninja management with options to view, add, and remove ninjas.

--- JavaNinja/src/NivelIntermediario/Desafio6/Desafio6.java
+++ JavaNinja/src/NivelIntermediario/Desafio6/Desafio6.java
@@ -1,5 +1,7 @@
 package NivelIntermediario.Desafio6;
 
+import NivelBasico.Arrays.Condicoes.SwitchCase;
+
 import java.util.Scanner;
 
 public class Desafio6 {
@@ -9,7 +11,7 @@ public class Desafio6 {
 
         Scanner sc = new Scanner(System.in);
         NinjaGenerico<Ninja> listaDeNinjas = new NinjaGenerico<>();
-
+        System.out.println("---------------cadastro inicial---------------".toUpperCase());
         for (int i = 0; i < 1; i++) {
             System.out.println("nome do ninja: ");
             String nome = sc.next();
@@ -18,15 +20,85 @@ public class Desafio6 {
             System.out.println("aldeia do ninja: ");
             String aldeia = sc.next();
 
-
             listaDeNinjas.colocarNinja(new Ninja(nome,idade,aldeia));
+        }
+        boolean continuarPrograma = true ;
+        
+//      LOOP DE MENU
+        
+        while(true){
+            if (!continuarPrograma) {
+                break;
+            }
+
+//          MENU INTERATIVO
+            
+            System.out.println("opções:".toUpperCase());
+            System.out.println("0- Ver lista.");
+            System.out.println("1- Remover o primeiro ninja.");
+            System.out.println("2- Adicionar ninja no inicio.");
+            System.out.println("3- Ver informações de um ninja.");
+            System.out.println("4- Sair.");
+            System.out.println("Digite o numero da opção aqui: ");
+            int opcao = sc.nextInt();
+
+            switch(opcao){
+
+                default:
+                    System.out.println("-------------- OPÇÃO INVALIDA -------------");
+                    System.out.println("por favor coloque uma opção valida!".toUpperCase());
+                    System.out.println("----------------- OPCÕES ------------------");
+                    break;
+
+                case 0:
+                    System.out.println("----------------- OPCÕES ------------------");
+                    listaDeNinjas.mostrarLista();
+                    System.out.println("----------------- OPCÕES ------------------");
+                    break;
+
+                case 1:
+                    System.out.println("----------------- REMOVER PRIMEIRO NINJA ------------------");
+                    listaDeNinjas.removerPrimeiroNinja();
+                    System.out.println("Ninja removido!!!".toUpperCase());
+                    System.out.println("Lista atualizada:".toUpperCase());
+                    listaDeNinjas.mostrarLista();
+                    System.out.println("----------------- OPCÕES ------------------");
+                    break;
+
+                case 2:
+                    System.out.println("----------------- ADICIONAR NINJA ------------------");
+                    System.out.println("nome do ninja: ");
+                    String nome = sc.next();
+                    System.out.println("idade do ninja: ");
+                    int idade = sc.nextInt();
+                    System.out.println("aldeia do ninja: ");
+                    String aldeia = sc.next();
+                    listaDeNinjas.adiconarNinjaInicio(new Ninja(nome,idade,aldeia));
+                    System.out.println("----------------- OPCÕES ------------------");
+                    break;
+
+                case 3:
+                    System.out.println("----------------- VER INFO ------------------");
+                    listaDeNinjas.mostrarLista();
+                    System.out.println("digite a posição do ninja que deseja acessar as informações: ");
+                    int linha = sc.nextInt();
+                    System.out.println("-----------------INFORMAÇÕES DO NINJA ------------------");
+                    listaDeNinjas.infoNinja(linha);
+                    System.out.println("----------------- OPCÕES ------------------");
+                    break;
+                case 4:
+                    System.out.println("----------- SAIR ------------");
+                    System.out.println("saindo do programa...".toUpperCase());
+                    continuarPrograma = false;
+                    break;
+                  }
+
+            }
+
 
 
         }
 
-        listaDeNinjas.mostrarLista();
-
-        listaDeNinjas.infoNinja(0);
     }
 
-}
+
